099cf4dfcd731453884161acdc8e5f0bc46f4f09
eshakkhan29 <eshakkhan29@gmail.com>
2022-03-06 10:40:27 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// setTimeout(() =>console.log("I am Superman"),1000)
+// console.log("I am Superwoman");
+
+
+
+const url = document.getElementById("anchor");
+const path = url.pathname;
+console.log(path);
